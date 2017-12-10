6cf1753128f33fe8fd0d6e04b1460ec45c64f3c7
469231573@qq.com <me452600>
2017-12-10 19:37:27 +0800

    增加student类

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,4 +1,6 @@
 from datetime import date
-from person import person
+from person import *
 p = person("张三",1,date(1990,8,14))
 print(p)
+s = student('张四',1,date(1990,1,2),"一年级1班")
+print(s)
--- PythonApplication1/person.py
+++ PythonApplication1/person.py
@@ -11,3 +11,11 @@ class person:
 		return '姓名是%s，性别是%s，出生日期是%s' % (self.name, self.get_gender(), self.birthdate)
 	def get_gender(self):
 		return '男' if self.gender else '女'
+
+class student(person):
+	classname=""
+	def __init__(self, name, gender, birthdate, classname):
+		self.classname = classname
+		return super().__init__(name, gender, birthdate)
+	def __str__(self):
+		return super().__str__() + '班级是%s' % self.classname
